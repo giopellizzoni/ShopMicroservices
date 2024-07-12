9aeb03595ca3761a6cede55d6e997e6ad74b1a96
Giovanni Pellizzoni <pelissoni@gmail.com>
2024-07-12 10:29:50 +0200

    Addin CreateProduct classes

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,37 @@
+﻿
+using BuildingBlocks.CQRS;
+
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public sealed record CreateProductCommand(
+    string Name,
+    List<string> Categories,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+public sealed record CreateProductResult(Guid id);
+
+
+internal sealed class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(
+        CreateProductCommand command,
+        CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //save to database
+
+        return new CreateProductResult(product.Id);
+
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,16 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Adding Services to the container.
-
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure HTTP Request Pipeline
+
+app.MapCarter();
 
 app.Run();
