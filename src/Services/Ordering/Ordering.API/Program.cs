26f2f8bb9df8c8d4662683d44234e20a4fb12c04
Giovanni Pellizzoni <pelissoni@gmail.com>
2024-07-30 10:42:38 +0200

    Implement Ordering.Domain with  DDD

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 var app = builder.Build();
 
+app.UseApiServices();
 
 app.Run();
