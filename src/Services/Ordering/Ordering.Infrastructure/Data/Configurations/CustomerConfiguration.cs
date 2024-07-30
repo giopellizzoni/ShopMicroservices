b7ddded057d54c1977c7f4b815e7e7afa6839e89
Giovanni Pellizzoni <pelissoni@gmail.com>
2024-07-30 19:27:06 +0200

    Adding configurations and DBContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id)
+            .HasConversion(customerId => customerId.Value,
+                dbId => CustomerId.Of(dbId));
+
+        builder.Property(c => c.Name).HasMaxLength(100);
+
+        builder.HasIndex(c => c.Email).IsUnique();
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        
+    }
+}
